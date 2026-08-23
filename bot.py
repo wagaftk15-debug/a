f3659c083a4afceebc4e59258801dc03b32fd9ba
wagaftk15-debug <wagaftk15@gmail.com>
2026-08-23 12:07:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 import psycopg2
-from flضask import Flask, request, jsonify, render_template_string
+from flask import Flask, request, jsonify, render_template_string
 
 app = Flask(__name__)
 
